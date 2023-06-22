26914a2c4ccbb27da6f4be61faa07e02b0ae6809
Sp3lllz <136860095+sp3lllz@users.noreply.github.com>
2023-06-22 21:41:53 +0100

    Update Add_2_Datto.ps1
    updated URL

--- Add_2_Datto.ps1
+++ Add_2_Datto.ps1
@@ -1,7 +1,7 @@
 #Add2Datto
 # v1.0
 # Written By Sp3lllz
-# https://github.com/sp3lllz/Sp3lllz_Admin_Toolkit
+# https://github.com/sp3lllz/Sp3lllz_Toolkit
 # Use Permitted under MIT licence
 
 # Prompt for user input
