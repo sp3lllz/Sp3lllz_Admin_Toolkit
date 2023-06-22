8698bb684f0c44da13ba2fdd44268f1f67da5579
Sp3lllz <136860095+sp3lllz@users.noreply.github.com>
2023-06-22 21:03:47 +0100

    Update Backup_FFXIV.ps1
    added ondrive agent start to navigate erros if user uses onedrive but agent isnt running

--- Backup_FFXIV.ps1
+++ Backup_FFXIV.ps1
@@ -11,6 +11,27 @@ $backupPath = Read-Host -Prompt "Enter the path you want to back up to:"
 #Get the Date
 $date = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
 $newFolder = "$backupPath\$date"
+$folders = Get-ChildItem -Path $backupPath -Directory
+ 
+# Prompt for OneDrive usage prompt user if they use onedrive 
+$useOneDrive = Read-Host -Prompt "Do you use OneDrive? (Y/N)"
+
+# Check if user uses OneDrive
+if ($useOneDrive -eq "Y" -or $useOneDrive -eq "y") {
+    # Check if OneDrive sync agent is running
+    $onedriveProcess = Get-Process -Name "OneDrive" -ErrorAction SilentlyContinue
+
+    if ($onedriveProcess -eq $null) {
+        Write-Host "OneDrive sync agent is not running. Starting it now..."
+        Start-Process -FilePath "C:\Program Files\Microsoft OneDrive\OneDrive.exe"
+    }
+    else {
+        Write-Host "OneDrive sync agent is already running."
+    }
+}
+else {
+    Write-Host "Skipping... Onedrive is not used"
+}
 
 #Make a new folder inside backup path with the date add time
 New-Item -ItemType Directory -Path $newFolder
@@ -20,3 +41,15 @@ New-Item -ItemType Directory -Path "$newFolder\game"
 #backup the data from the provided paths to the back up path 
 robocopy $pluginsPath "$newFolder\plugins" /E /COPY:DAT /R:1 /W:1
 robocopy $gamePath "$newFolder\game" /E /COPY:DAT /R:1 /W:1
+
+#compress the backups to a zip file and delete the uncompressed backup 
+Compress-Archive -Path "$newFolder\plugins" -DestinationPath "$newFolder\plugins.zip"
+Remove-Item "$newFolder\plugins" -Recurse
+Compress-Archive -Path "$newFolder\game" -DestinationPath "$newFolder\game.zip"
+Remove-Item "$newFolder\game" -Recurse
+foreach ($folder in $folders) {
+    $folderDate = (Get-Item $folder.FullName).CreationTime
+    $age = New-TimeSpan -Start $folderDate -End $currentDate
+    if ($age.TotalDays -gt 365) {
+        Remove-Item -Path $folder.FullName -Recurse -Force
+    }
