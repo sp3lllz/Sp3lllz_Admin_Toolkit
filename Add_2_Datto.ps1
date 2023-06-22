974e5ff58190a584e3e4590b7971a3789a31b8a7
Sp3lllz <136860095+sp3lllz@users.noreply.github.com>
2023-06-22 20:14:25 +0100

    uploaded Add_2_Datto

---
+++ Add_2_Datto.ps1
@@ -0,0 +1,11 @@
+#Add2Datto
+# v1.0
+# Written By Sp3lllz
+# https://github.com/sp3lllz/Sp3lllz_Admin_Toolkit
+# Use Permitted under MIT licence
+
+# Prompt for user input
+$dattourl = Read-Host -Prompt "Enter a a datto install url:"
+
+
+(New-Object System.Net.WebClient).DownloadFile($dattourl, "$env:TEMP/AgentInstall.exe");start-process "$env:TEMP/AgentInstall.exe"
