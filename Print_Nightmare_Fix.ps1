211e20c6f3aebc1d2047c6bfc59a00bb9741c303
Sp3lllz <136860095+sp3lllz@users.noreply.github.com>
2023-06-22 21:44:45 +0100

    Update Print_Nightmare_Fix.ps1
    updated URL

--- Print_Nightmare_Fix.ps1
+++ Print_Nightmare_Fix.ps1
@@ -1,7 +1,7 @@
 # Print Nightmare Fix
 # v1.0
 # Written By Sp3lllz
-# https://github.com/sp3lllz/Sp3lllz_Admin_Toolkit
+# https://github.com/sp3lllz/Sp3lllz_Toolkit
 # Use Permitted under MIT licence
 
 New-Item -Path "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Print" -Name "RpcAuthnLevelPrivacyEnabled" -Value 0 -PropertyType DWORD
